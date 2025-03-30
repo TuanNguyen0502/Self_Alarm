4e8c2d324fb55b939e2fddbba82afc530d574f98
loh <nguyenhahongtuan@gmail.com>
2025-03-30 21:27:25 +0700

    feat: implementation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,8 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.3.1")
     implementation("androidx.compose.material3:material3-window-size-class:1.3.1")
     implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.4.0-alpha10")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.palette:palette:1.0.0")
+    implementation("androidx.media:media:1.1.0")
 }
